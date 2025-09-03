8797544cdd0946ccf9ea009f3055e6d4b06631f3
ara fa adri <arafa.sco@gmail.com>
2025-09-03 11:41:54 +0700

    fixs: trouble tom select

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+// import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "localhost",
+    port: 4000,
+  },
   build: {
     commonjsOptions: {
       include: ["tailwind.config.js", "node_modules/**"],
